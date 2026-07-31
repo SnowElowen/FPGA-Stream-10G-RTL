5030819040cdd88d4f43ff3351c73f06f6504b76
ZiXing Jiang <ruansheng333@gmail.com>
2026-07-31 18:24:56 +0800

    Add RTL file manifest

---
+++ rtl/files.f
@@ -0,0 +1,8 @@
+rtl/common/axis_elastic_buffer.sv
+rtl/common/axis_frame_buffer.sv
+rtl/common/ones_complement_checksum.sv
+rtl/rx/ipv4_udp_rx.sv
+rtl/tx/ipv4_udp_tx.sv
+rtl/arp/arp_rx.sv
+rtl/arp/arp_tx.sv
+rtl/fpga_stream10g_core.sv
